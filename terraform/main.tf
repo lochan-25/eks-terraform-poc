918584a8f9c2c9397e15905aadc788859f260249
lochan-25 <ramamachavolu1999@gmail.com>
2026-06-02 13:49:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,49 +1,60 @@
-module "eks" {
-
-source = "terraform-aws-modules/eks/aws"
-
-cluster_name="demo-eks"
-
-cluster_version="1.31"
-
-subnet_ids = module.vpc.private_subnets
-
-vpc_id = module.vpc.vpc_id
-
-eks_managed_node_groups = {
-
-one = {
-
-instance_types=["t3.small"]
-
-min_size=1
-
-max_size=2
-
-desired_size=1
-
-}
-
-}
-
-}
-
 module "vpc" {
 
-source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
 
-name="eks-vpc"
+  name = "eks-vpc"
 
-cidr="10.0.0.0/16"
+  cidr = "10.0.0.0/16"
 
-azs=["us-east-1a","us-east-1b"]
+  azs = ["us-east-1a","us-east-1b"]
 
-private_subnets=["10.0.1.0/24","10.0.2.0/24"]
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
 
-public_subnets=["10.0.101.0/24","10.0.102.0/24"]
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
 
-enable_nat_gateway=true
+  enable_nat_gateway = true
 
-single_nat_gateway=true
+  single_nat_gateway = true
+
+}
+
+
+
+module "eks" {
+
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name = "demo-eks"
+
+  kubernetes_version = "1.31"
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+
+  eks_managed_node_groups = {
+
+    default = {
+
+      instance_types = ["t3.small"]
+
+      min_size = 1
+
+      max_size = 1
+
+      desired_size = 1
+
+    }
+
+  }
 
 }
