4de865803fd66f62fe001f61614447498dcbac8f
lochan-25 <ramamachavolu1999@gmail.com>
2026-06-02 18:43:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,10 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs = ["us-east-1a","us-east-1b"]
+  azs = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
 
   private_subnets = [
     "10.0.1.0/24",
@@ -23,19 +26,27 @@ module "vpc" {
 
   single_nat_gateway = true
 
-}
+  enable_dns_hostnames = true
 
+  enable_dns_support = true
+
+}
 
 
 
 module "eks" {
 
   source  = "terraform-aws-modules/eks/aws"
+
   version = "~> 21.0"
 
   name = "demo-eks"
 
   kubernetes_version = "1.31"
+
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id = module.vpc.vpc_id
 
@@ -46,7 +57,7 @@ module "eks" {
 
     default = {
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
 
       min_size = 1
 
@@ -54,8 +65,18 @@ module "eks" {
 
       desired_size = 1
 
+      ami_type = "AL2023_x86_64_STANDARD"
+
     }
 
   }
+
+}
+
+
+
+output "cluster_name" {
+
+ value = module.eks.cluster_name
 
 }
