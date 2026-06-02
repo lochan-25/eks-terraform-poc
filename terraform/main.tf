9c0dcceb516a6b62ab05a8b1f6fde2240ee6d0fa
lochan-25 <ramamachavolu1999@gmail.com>
2026-06-02 18:45:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,13 +44,11 @@ module "eks" {
 
   kubernetes_version = "1.31"
 
-  cluster_endpoint_public_access = true
-
-  enable_cluster_creator_admin_permissions = true
-
   vpc_id = module.vpc.vpc_id
 
   subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
 
 
   eks_managed_node_groups = {
