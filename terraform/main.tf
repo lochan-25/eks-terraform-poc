5a075237f562560374c8ab9d8999ee8b1ee4c52b
lochan-25 <ramamachavolu1999@gmail.com>
2026-06-03 12:30:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 module "vpc" {
 
   source  = "terraform-aws-modules/vpc/aws"
@@ -12,14 +14,14 @@ module "vpc" {
     "us-east-1b"
   ]
 
-  private_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
   public_subnets = [
     "10.0.101.0/24",
     "10.0.102.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
   ]
 
   enable_nat_gateway = true
@@ -32,7 +34,6 @@ module "vpc" {
 
   map_public_ip_on_launch = true
 
-
   public_subnet_tags = {
 
     "kubernetes.io/role/elb" = "1"
@@ -40,7 +41,6 @@ module "vpc" {
     "kubernetes.io/cluster/demo-eks" = "shared"
 
   }
-
 
   private_subnet_tags = {
 
@@ -52,8 +52,6 @@ module "vpc" {
 
 }
 
-
-
 module "eks" {
 
   source  = "terraform-aws-modules/eks/aws"
@@ -64,11 +62,40 @@ module "eks" {
 
   kubernetes_version = "1.31"
 
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id = module.vpc.vpc_id
 
   subnet_ids = module.vpc.public_subnets
 
-  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+
+    admin = {
+
+      principal_arn = data.aws_caller_identity.current.arn
+
+      policy_associations = {
+
+        admin = {
+
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+
+            type = "cluster"
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
 
 
   eks_managed_node_groups = {
@@ -85,13 +112,13 @@ module "eks" {
 
       ami_type = "AL2023_x86_64_STANDARD"
 
+      subnet_ids = module.vpc.public_subnets
+
     }
 
   }
 
 }
-
-
 
 output "cluster_name" {
 
