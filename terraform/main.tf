c719f1d988b9456a54854d9ff39851af34a41aff
lochan-25 <ramamachavolu1999@gmail.com>
2026-06-03 12:33:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,6 +52,8 @@ module "vpc" {
 
 }
 
+
+
 module "eks" {
 
   source  = "terraform-aws-modules/eks/aws"
@@ -61,8 +63,6 @@ module "eks" {
   name = "demo-eks"
 
   kubernetes_version = "1.31"
-
-  cluster_endpoint_public_access = true
 
   enable_cluster_creator_admin_permissions = true
 
@@ -119,6 +119,8 @@ module "eks" {
   }
 
 }
+
+
 
 output "cluster_name" {
 
